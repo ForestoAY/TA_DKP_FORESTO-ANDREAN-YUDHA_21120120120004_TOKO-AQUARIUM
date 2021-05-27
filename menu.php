24460b6be3d6090e0cdc55046e26188e8d907947
Foresto Andrean Yudha <82255137+ForestoAY@users.noreply.github.com>
2021-05-27 12:11:28 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Tugas Akhir Praktikum DKP</title>
-<head>
+    <head>
+        <title>Tugas Akhir Praktikum DKP</title>
+    <head>
 
-<body>
-    <caption>Aquarium Store</caption><hr>
-       Silahkan pilih menu: <br>
-    <form action="beli.php" method="get">
-        <input type="submit" name="Beli" value="Buat pesanan">
-    </form>
-    <br>
-    <form action="ikan.php" method="get">
-        <input type="submit" name="Lihat ikan" value="Lihat ikan">
-    </form>
-</body>
+    <body>
+        <caption>Aquarium Store</caption><hr>
+           Silahkan pilih menu: <br>
+        <form action="beli.php" method="get">
+            <input type="submit" name="Beli" value="Buat pesanan">
+        </form>
+        <br>
+        <form action="ikan.php" method="get">
+            <input type="submit" name="Lihat ikan" value="Lihat ikan">
+        </form>
+    </body>
+</html>
