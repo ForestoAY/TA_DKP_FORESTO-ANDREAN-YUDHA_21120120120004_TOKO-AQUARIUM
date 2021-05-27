bb2b47e9b69eba6e0bf8f77ce40c600c2ddcb5c8
ForestoAY <forestoay@gmail.com>
2021-05-27 12:06:12 +0700

    Menu utama

---
+++ menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tugas Akhir Praktikum DKP</title>
+<head>
+
+<body>
+    <caption>Aquarium Store</caption><hr>
+       Silahkan pilih menu: <br>
+    <form action="beli.php" method="get">
+        <input type="submit" name="Beli" value="Buat pesanan">
+    </form>
+    <br>
+    <form action="ikan.php" method="get">
+        <input type="submit" name="Lihat ikan" value="Lihat ikan">
+    </form>
+</body>
