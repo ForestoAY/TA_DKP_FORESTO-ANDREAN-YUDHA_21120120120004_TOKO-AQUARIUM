bfb355b984cdb5784fd55aec5405764a1e9aad98
Foresto Andrean Yudha <82255137+ForestoAY@users.noreply.github.com>
2021-05-29 20:13:29 +0700

    Update beli.php

--- beli.php
+++ beli.php
@@ -2,45 +2,75 @@
 <html>
 <head>
     <title>Tugas Akhir Praktikum DKP</title>
+    <link rel="stylesheet" href="style.css">
 <head>
 
 <body>
-    <caption>Aquarium Store</caption><hr>
-    <form action="hasil.php" method="get">  
-        <label for="nama">Nama:</label>
-            <input name="nama" >
-            <br><br>
-            <label for="alamat">Alamat :</label>
-            <input name="alamat">
-            <br><br>
-        <tr>
-        <td>Total Saldo : </td>
-        <td><input type="number" name="saldo"></td>
-        <br><br>
-        </tr>
-        <label for="jk">Jenis Kelamin :</label></td>
-            <Select name="jk" id="jk">
-            <option value='pria'>Pria</option>
-            <option value='wanita'>Wanita</option>
-            </Select><br><br>
-        <tr>
-            <td><label for="pesanan">Pesanan Anda :</label>
-            <Select name="pesanan" id="pesanan">
-            <option value='Ikan Cupang'>Ikan Cupang - Rp 1.000</option>
-            <option value='Ikan Guppy'>Ikan Guppy - Rp 1.500</option>
-            <option value='Ikan Mas Koki'>Ikan Mas Koki - Rp 10.000</option>
-            <option value='Ikan Koi'>Ikan Koi - Rp 20.000</option>
-            <option value='Ikan Discus'>Ikan Discus - Rp 75.000</option>
-            <option value='Ikan Arwana'>Ikan Arwana - Rp 200.000</option>
-            </Select><br><br></td>
-        </tr>
-        <tr>
-            <td>Jumlah : </td><input type="number" name="jumlah"></tr><br><br>
-        <input type="submit" name="Pesan">
-        <input type="reset" value="Pesan ulang"></tr>
-    </form>
-    <br>
-    <form action="menu.php" method="get">
-        <input type="submit" name="Kembali" value="Kembali">
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href=''>Aquarium Store</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#home">Home</a></li>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#lihatikan">Lihat ikan</a></li>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#pesanan">Pesanan</a></li>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#kontak">Kontak</a></li>
+                </ul>
+            </div>
+    </nav>
+    <form action="hasil.php" method="post">  
+                Nama:
+                <input type="text" name="nama" id="nama">
+                <?php
+                    if(isset($_GET['errnama'])) {
+                        $err1 = $_GET['errnama'];
+                        echo $err1;
+                    }
+                ?>
+                    <br><br>
+                Alamat:
+                <input type="text" name="alamat" id="alamat">
+                <?php
+                    if(isset($_GET['erralamat'])) {
+                        $err2 = $_GET['erralamat'];
+                        echo $err2;
+                    }
+                ?>
+                    <br><br>
+                    <label for="saldo">Total Saldo :</label>
+                    <input type="number" name="saldo" id="saldo">
+                    <?php
+                    if(isset($_GET['errsaldo'])) {
+                        $err3 = $_GET['errsaldo'];
+                        echo $err3;
+                    }
+                ?>
+                    <br><br>
+                    <label for="jk">Jenis Kelamin :</label>
+                        <Select name="jk" id="jk">
+                        <option value='pria'>Pria</option>
+                        <option value='wanita'>Wanita</option>
+                        </Select><br><br>
+                    <label for="pesanan">Pesanan Anda :</label>
+                        <Select name="pesanan" id="pesanan">
+                        <option value='Ikan Cupang'>Ikan Cupang - Rp 1.000</option>
+                        <option value='Ikan Guppy'>Ikan Guppy - Rp 1.500</option>
+                        <option value='Ikan Mas Koki'>Ikan Mas Koki - Rp 10.000</option>
+                        <option value='Ikan Koi'>Ikan Koi - Rp 20.000</option>
+                        <option value='Ikan Discus'>Ikan Discus - Rp 75.000</option>
+                        <option value='Ikan Arwana'>Ikan Arwana - Rp 200.000</option>
+                        </Select><br><br>
+                    Jumlah :
+                    <input type="number" name="jumlah">
+                        <?php 
+                            if(isset($_GET["errjumlah"])) {
+                                $err4 = $_GET["errjumlah"];
+                                echo $err4;
+                            }
+                        ?>
+                        <br><br>
+        <input type="submit" name="Pesan" value="Pesan sekarang">
+        <input type="reset" value="Pesan ulang">
     </form>
 </body>
+</html>
