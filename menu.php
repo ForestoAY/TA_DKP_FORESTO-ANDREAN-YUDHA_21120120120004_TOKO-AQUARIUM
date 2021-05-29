5e43514a43f71f4e531de08133125c69f718f102
Foresto Andrean Yudha <82255137+ForestoAY@users.noreply.github.com>
2021-05-29 20:15:06 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,18 +1,98 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Tugas Akhir Praktikum DKP</title>
-    <head>
+<head>
+    <title>Tugas Akhir Praktikum DKP</title>
+    <link rel="stylesheet" href="style.css">
+<head>
 
-    <body>
-        <caption>Aquarium Store</caption><hr>
-           Silahkan pilih menu: <br>
-        <form action="beli.php" method="get">
-            <input type="submit" name="Beli" value="Buat pesanan">
-        </form>
-        <br>
-        <form action="ikan.php" method="get">
-            <input type="submit" name="Lihat ikan" value="Lihat ikan">
-        </form>
-    </body>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href=''>Aquarium Store</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#lihatikan">Lihat ikan</a></li>
+                    <li><a href="#pesanan">Pesanan</a></li>
+                    <li><a href="#kontak">Kontak</a></li>
+                </ul>
+            </div>
+    </nav>
+    <div class="class">
+        <section id="home">
+            <img src="https://s3.gifyu.com/images/aquarium.jpg"/>
+            <div class="kolom">
+                <p class="deskripsi">Jual Ikan Hias murah hanya di Aquarium Store</p>
+                <h2>Selamat Berbelanja</h2>
+            </div>
+        </section>
+
+        <section id="lihatikan">
+            <div class="kolom">
+                <p class="deskripsi">Jenis ikan yang kami jual</p>
+                <h2>Ikan Cupang</h2>
+                <img src="https://s3.gifyu.com/images/ikan-cupang.jpg"/>
+                <h2>Ikan Guppy</h2>
+                <img src="https://s3.gifyu.com/images/ikan-guppy.jpg"/>
+                <h2>Ikan Mas Koki</h2>
+                <img src="https://s3.gifyu.com/images/ikan-mas-koki.jpg"/>
+                <h2>Ikan Koi</h2>
+                <img src="https://s3.gifyu.com/images/ikan-koi.jpg"/>
+                <h2>Ikan Discus</h2>
+                <img src="https://s3.gifyu.com/images/ikan-discus.md.jpg"/>
+                <h2>Ikan Arwana</h2>
+                <img src="https://s3.gifyu.com/images/ikan-arwana.jpg"/>
+            </div>
+        
+        </section>
+
+        <section id="pesanan">
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">Ingin beli ? Yok langsung dipesan dengan klik tombol dibawah ini !</p>
+                <div class="menupesanan">
+                    <p><a href="http://localhost/Tugas%20Akhir%20PDKP/beli.php" class="tekan">Pesan sekarang</a></p>
+                    
+                    </div>
+                </div>
+            </div>
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">Penasaran dengan harganya ? Yok di cek sekarang</p>
+                <div class="menupesanan">
+                    <p><a href="http://localhost/Tugas%20Akhir%20PDKP/ikan.php" class="tekan">Cek harga</a></p>
+                    
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="kontak">
+            <div class="kontak">
+                <div class="footer">
+                    <div class="footer-section">
+                        <h3>Aquarium Store</h3>
+                        <p>Menjual ikan hias murah</p>
+                    </div>
+
+                    <div class="footer-section">
+                        <h3>About</h3>
+                        <p>Foresto Andrean Yudha sebagai praktikan PDKP 2021</p>
+                    </div>
+
+                    <div class="footer-section">
+                        <h3>Contact</h3>
+                        <p>Foresto Andrean Yudha - 082384687770</p>
+                    </div>
+
+                    <div class="footer-section">
+                        <h3>Social</h3>
+                        <p>Line - forestoandreany</p>
+                        <p>Instagram - Foresto Andrean Yudha</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
 </html>
