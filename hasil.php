3627bcc66a651b0980cfb56c8d54fba1d65f8ae0
Foresto Andrean Yudha <82255137+ForestoAY@users.noreply.github.com>
2021-05-29 20:14:14 +0700

    Update hasil.php

--- hasil.php
+++ hasil.php
@@ -2,40 +2,76 @@
 <html>
 <head>
     <title>Tugas Akhir Praktikum DKP</title>
+    <link rel="stylesheet" href="style.css">
 <head>
-    
-<body>
-<?php $nama=$_GET['nama']; $alamat=$_GET['alamat']; ?>
 
-<table border="1">
-    <tr>
-        <th colspan="2">Bukti pembelian</th>
-    </tr>
-    <tr>
-        <td>Saldo Awal</td>
-        <td><?php echo $saldo=$_GET['saldo']; ?></td>
-    </tr>
-    <tr>
-        <td>Pesanan</td>
-        <td><?php echo $pesanan=$_GET['pesanan']; ?></td>
-    </tr>
-    <tr>
-        <td>Jumlah</td>
-        <td><?php echo $jumlah=$_GET['jumlah']; ?></td>
-    </tr>
-    <tr>
-        <td>Saldo Akhir</td>
-        <td><?php
-        //pengkondisian apabila kosong
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href=''>Aquarium Store</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#home">Home</a></li>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#lihatikan">Lihat ikan</a></li>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#pesanan">Pesanan</a></li>
+                    <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#kontak">Kontak</a></li>
+                </ul>
+            </div>
+    </nav>
+        <?php 
+            session_start();
+            $nama=$_POST['nama']; 
+            $alamat=$_POST['alamat']; 
+            $jk = $_POST['jk']; 
+            $saldo=$_POST['saldo'];
+            $pesanan=$_POST['pesanan']; 
+            $jumlah=$_POST['jumlah'];
+
+            if (empty($nama)) {
+                header("location:beli.php?errnama= * Nama belum di isi");
+            }
+            elseif (empty($alamat)) {
+                header("location:beli.php?erralamat= * Alamat belum di isi");
+            }
+            elseif (empty($saldo)) {
+                header("location:beli.php?errsaldo= * Saldo belum di isi");
+            }
+            elseif (empty($jumlah)) {
+                header("location:beli.php?errjumlah= * Jumlah belum di isi");
+            }
+            else {
+
+            }
+        ?>
+
+        <table border="1">
+            <tr>
+                <th colspan="2">Bukti pembelian</th>
+            </tr>
+            <tr>
+                <td>Saldo Awal</td>
+                <td><?php echo $saldo; ?></td>
+            </tr>
+            <tr>
+                <td>Pesanan</td>
+                <td><?php echo $pesanan; ?></td>
+            </tr>
+            <tr>
+                <td>Jumlah</td>
+                <td><?php echo $jumlah; ?></td>
+            </tr>
+            <tr>
+                <td>Saldo Akhir</td>
+                <td><?php
         if(empty($jumlah && $saldo)){
             echo "Mohon diisi dengan benar";
-         }
-         else{
+        }
+        else{
             //function
             function hasil($saldo, $harga, $jumlah){
                 return $saldo - $harga * $jumlah;
             }
- 
+
             //harga pesanan
             if($pesanan == 'Ikan Cupang'){
                 $harga = 1000;
@@ -57,36 +93,37 @@
             }
             $hasil = hasil($saldo, $harga, $jumlah);
             echo $hasil;
-         }
+        }
         ?></td>
-</table> <br>
-<?php
-    if($saldo<=0){
-        echo "Saldo kurang, pemesanan gagal.";
-        }
-        elseif($jumlah<=0){
-            echo "Maaf, Anda belum memasukkan jumlah.";
-        }
-        elseif($hasil<0){
-            echo "Maaf uang Anda kurang, pemesanan gagal.";
-        }
-        else{
-            if(empty($nama && $alamat)){
-                echo "Pengiriman gagal, mohon nama dan alamat diisi dengan lengkap.";
-            }
-            else{
-                $jk = $_GET['jk'];
-                if($jk == 'pria')
-                {
-                $welcome = 'Terima kasih telah melakukan pemesanan, mas '.$nama; 
+        </table> <br>
+        <?php
+            if($saldo<=0){
+                echo "Saldo kurang, pemesanan gagal.";
                 }
-                else 
-                {
-                $welcome = 'Terima kasih telah melakukan pemesanan, mbak '.$nama;
-                }             
-                echo $welcome.". ";
-                echo "Pesanan akan dikirim ke ".$alamat.".";
-            }
-        }
-?>
+                elseif($jumlah<=0){
+                    echo "Maaf, Anda belum memasukkan jumlah.";
+                }
+                elseif($hasil<0){
+                    echo "Maaf uang Anda kurang, pemesanan gagal.";
+                }
+                else{
+                    if(empty($nama && $alamat)){
+                        echo "Pengiriman gagal, mohon nama dan alamat diisi dengan lengkap.";
+                    }
+                    else{
+                        if($jk == 'pria')
+                        {
+                        $welcome = 'Terima kasih telah melakukan pemesanan, mas '.$nama; 
+                        }
+                        else 
+                        {
+                        $welcome = 'Terima kasih telah melakukan pemesanan, mbak '.$nama;
+                        }             
+                        echo $welcome.". ";
+                        echo "Pesanan akan dikirim ke ".$alamat.".";
+                    }
+                }
+        ?>
 </br>
+</body>
+</html>
