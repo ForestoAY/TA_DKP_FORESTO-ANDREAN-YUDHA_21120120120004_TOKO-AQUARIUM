8dcc96e5901d1168339a8fd0f943aae9fc7fb0c7
Foresto Andrean Yudha <82255137+ForestoAY@users.noreply.github.com>
2021-06-01 09:19:58 +0700

    Update beli.php

--- beli.php
+++ beli.php
@@ -2,13 +2,13 @@
 <html>
 <head>
     <title>Tugas Akhir Praktikum DKP</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleweb.css">
 <head>
 
 <body>
     <nav>
         <div class="wrapper">
-            <div class="logo"><a href=''>Aquarium Store</a></div>
+            <div class="logo"><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php">Aquarium Store</a></div>
             <div class="menu">
                 <ul>
                     <li><a href="http://localhost/Tugas%20Akhir%20PDKP/menu.php#home">Home</a></li>
@@ -18,9 +18,10 @@
                 </ul>
             </div>
     </nav>
+    <br>
     <form action="hasil.php" method="post">  
                 Nama:
-                <input type="text" name="nama" id="nama">
+                <input type="text" name="nama" id="nama" placeholder="Masukkan Nama">
                 <?php
                     if(isset($_GET['errnama'])) {
                         $err1 = $_GET['errnama'];
@@ -29,7 +30,7 @@
                 ?>
                     <br><br>
                 Alamat:
-                <input type="text" name="alamat" id="alamat">
+                <input type="text" name="alamat" id="alamat" placeholder="Masukkan Alamat">
                 <?php
                     if(isset($_GET['erralamat'])) {
                         $err2 = $_GET['erralamat'];
@@ -38,7 +39,7 @@
                 ?>
                     <br><br>
                     <label for="saldo">Total Saldo :</label>
-                    <input type="number" name="saldo" id="saldo">
+                    <input type="number" name="saldo" id="saldo" placeholder="Masukkan Saldo">
                     <?php
                     if(isset($_GET['errsaldo'])) {
                         $err3 = $_GET['errsaldo'];
@@ -61,7 +62,7 @@
                         <option value='Ikan Arwana'>Ikan Arwana - Rp 200.000</option>
                         </Select><br><br>
                     Jumlah :
-                    <input type="number" name="jumlah">
+                    <input type="number" name="jumlah" placeholder="Masukkan Jumlah">
                         <?php 
                             if(isset($_GET["errjumlah"])) {
                                 $err4 = $_GET["errjumlah"];
